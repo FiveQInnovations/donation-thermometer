15901532f5f3a3bd451f2f4641705da9b4c2c877
Anthony Elliott <anthony.elliott@fiveq.com>
2022-07-16 10:41:21 -0500

    250k

--- donation-thermometer.js
+++ donation-thermometer.js
@@ -10,7 +10,7 @@ function getGoalAmount() {
 function getCurrentAmount() {
     // Put code here to get the current amount
     console.log('running getCurrentAmount');
-    return 90.8888888;
+    return 250000;
 }
 
 var data = { 
