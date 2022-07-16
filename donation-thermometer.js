ed432925d554b9e2772d7ac0a88f513ec9df56ce
Anthony Elliott <anthony.elliott@fiveq.com>
2022-07-16 09:33:14 -0500

    logging

--- donation-thermometer.js
+++ donation-thermometer.js
@@ -1,12 +1,15 @@
+// Upload this to AWS S3
 console.log('in donation-thermometer.js');
 
 function getGoalAmount() {
     // Put code here to get the goal amount
+    console.log('running getGoalAmount');
     return 100;
 }
 
 function getCurrentAmount() {
     // Put code here to get the current amount
+    console.log('running getCurrentAmount');
     return 5;
 }
 
@@ -16,7 +19,3 @@ var data = {
 };
 console.log('posting message');
 window.parent.postMessage(data, '*')
-
-export function setCurrentAmount(newCurrentAmount) {
-    // do stuff
-}
--- upload.sh
+++ upload.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
-echo 'Uploading to S3'
-aws s3 cp donation-thermometer.js s3://journity-scripts/anthony-test/
-aws s3 cp iframe.html s3://journity-scripts/anthony-test/
-echo 'Done'
+aws s3 cp donation-thermometer.js s3://journity-scripts/anthony-test/ --acl public-read
+aws s3 cp iframe.html s3://journity-scripts/anthony-test/ --acl public-read
