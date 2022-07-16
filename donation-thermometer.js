541e1a57b310ec102199e833c3152edf9bed9c2d
Anthony Elliott <anthony.elliott@fiveq.com>
2022-07-16 10:53:22 -0500

    pick option 1: API call

--- donation-thermometer.js
+++ donation-thermometer.js
@@ -3,10 +3,13 @@ console.log('in donation-thermometer.js');
 
 function getGoalAmount() {
     // Put code here to get the goal amount
-    // Option 1: API call
+    // Option 1: API call - do this one
     // Option 2: scrape parent document
+        // Need to put data into parent doc in more consistent pattern
     // Option 3: interact with function somehow
+        // Denied by browser due to security issues
     // Option 4: Client manually updates this file
+        // Possible, but not worthwhile
 
     console.log('running getGoalAmount');
     return 10**6;
