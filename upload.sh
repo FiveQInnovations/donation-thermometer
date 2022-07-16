2a85547d132057aa8184b4c797dbb31ed2b9c9ce
Anthony Elliott <anthony.elliott@fiveq.com>
2022-07-16 11:44:07 -0500

    move unneeded files

--- upload.sh
+++ upload.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 aws s3 cp donation-thermometer.js s3://journity-scripts/anthony-test/ --acl public-read
 aws s3 cp iframe.html s3://journity-scripts/anthony-test/ --acl public-read
-aws s3 cp client-file.js s3://journity-scripts/anthony-test/ --acl public-read
