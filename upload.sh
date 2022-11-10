d9e348ede6c0843d367756bb70dcf6eac47cd1d5
Anthony Elliott <anthony.elliott@fiveq.com>
2022-11-10 07:16:32 -0600

    add manual cachebusting

--- upload.sh
+++ upload.sh
@@ -2,3 +2,4 @@
 aws s3 cp donation-thermometer.js s3://journity-scripts/frc/ --acl public-read
 aws s3 cp iframe.html s3://journity-scripts/frc/ --acl public-read
 aws s3 cp donation-thermometer.css s3://journity-scripts/frc/ --acl public-read
+aws s3 cp assets/* s3://journity-scripts/frc/assets/ --acl public-read
