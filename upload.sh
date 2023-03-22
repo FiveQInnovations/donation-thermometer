8ff40434cea972812f2ae1ef265d7b7cbe35a48e
Anthony Elliott <anthony.elliott@fiveq.com>
2023-03-22 10:45:22 -0500

    use non-client specific urls for example

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-aws s3 cp donation-thermometer.js s3://journity-scripts/frc/ --acl public-read
-aws s3 cp iframe.html s3://journity-scripts/frc/ --acl public-read
-aws s3 cp donation-thermometer.css s3://journity-scripts/frc/ --acl public-read
-aws s3 cp --recursive assets/ s3://journity-scripts/frc/assets/ --acl public-read
+aws s3 cp donation-thermometer.js s3://journity-scripts/thermometer/ --acl public-read
+aws s3 cp iframe.html s3://journity-scripts/thermometer/ --acl public-read
+aws s3 cp donation-thermometer.css s3://journity-scripts/thermometer/ --acl public-read
+aws s3 cp --recursive assets/ s3://journity-scripts/thermometer/assets/ --acl public-read
